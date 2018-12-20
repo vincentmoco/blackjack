04c8f69bc842a1c709e0c49840f59897a4c7e07c
Vincent Moco <Vincentmoco@MacBook-Pro-de-Vincent.local>
2018-12-20 21:25:18 +0100

    init files

--- blackjack.js
+++ blackjack.js
@@ -18,14 +18,16 @@ le jeu blackjack
     divJS.appendChild(newImg);
 }
 
-
- var change = function changeImg(liste_chemin){
-	var ext = '.BMP';
-	var racine = '/Users/mocovincent/Documents/2018-2019/blackjack/img/';
+var generer = function generer_liste(){
 	for (i = 1; i <= 52; i++) {	
 		var chemin = i;
 		liste_chemin.push(chemin)	
 	}
+	return liste_chemin
+}
+ var change = function changeImg(liste_chemin){
+	var ext = '.BMP';
+	var racine = '/Users/mocovincent/Documents/2018-2019/blackjack/img/';
 	var val_carte = liste_chemin[Math.floor(Math.random()*liste_chemin.length)];
 	var carte_joueur =( racine + val_carte.toString() + ext);
 	liste_chemin.splice(val_carte-1,1);
@@ -37,17 +39,19 @@ liste_chemin.splice(val_carte-1) pour eviter les doublons mais il y a un bug qua
 */
 
 //PROGRAMME PRINCIPALE
-var liste_chemin = [];
 var element = document.getElementById('ajout_joueur');	
 var element2 = document.getElementById('ajout_banque');	
-
 var carte_depart_banque = document.getElementById('carte_1');
 var carte_depart_joueur = document.getElementById('carte_2');
 
+var liste_chemin = [];
+liste_chemin = generer();
+
+//choix aleatoire des cartes au début
 carte_depart_banque.src=change(liste_chemin);
 carte_depart_joueur.src=change(liste_chemin); 
 
-
+// bouton carte joueur
 element.addEventListener('click',function ajoutImgDansDiv() {
     var chemin = change(liste_chemin);
     var newImg = creer(chemin);
@@ -55,7 +59,7 @@ element.addEventListener('click',function ajoutImgDansDiv() {
     divJS.appendChild(newImg);
 });
 
-	
+// bouton carte banque	
 element2.addEventListener('click',function ajoutImgDansDiv() {
     var chemin = change(liste_chemin);
     var newImg = creer(chemin);
