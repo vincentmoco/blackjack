487101c367f8cd0832336eb4d79ab2e49fbfb75c
Vincent Moco <Vincentmoco@MacBook-Pro-de-Vincent.local>
2018-12-21 18:57:09 +0100

    init files

--- blackjack.js
+++ blackjack.js
@@ -16,43 +16,44 @@ var generer = function generer_liste(){
 		var chemin = i;
 		liste_chemin.push(chemin)	
 	}
-	return liste_chemin
+	return liste_chemin;
 }
+
  
-var change = function changeImg(liste_chemin){
+
+var change = function changeImg(liste_chemin) {
 	var ext = '.BMP';
 	var racine = '/Users/mocovincent/Documents/2018-2019/blackjack/img/';
 	var val_carte = liste_chemin[Math.floor(Math.random()*liste_chemin.length)];
-	var carte_joueur =( racine + val_carte.toString() + ext);
-	liste_chemin.splice(val_carte-1,1);
+	points.valeur = val_carte;
+	var carte_joueur =(racine + val_carte.toString() + ext);
 	return carte_joueur;
 }
-//test
- var ajout = function ajoutImgDansDiv(change,element) {
-    var chemin = change(liste_chemin); 
-    var newImg = creerImg(chemin);
-	var divJS = element;
-    divJS.appendChild(newImg);
-}
 
 
-
-/*
-liste_chemin.splice(val_carte-1) pour eviter les doublons mais il y a un bug quand meme
-*/
 
 //PROGRAMME PRINCIPALE
 var element1 = document.getElementById('ajout_joueur');	
 var element2 = document.getElementById('ajout_banque');	
 var carte_depart_banque = document.getElementById('carte_1');
+
 var carte_depart_joueur = document.getElementById('carte_2');
+var points_joueur = document.getElementById('points_joueur');
+var points =  {valeur : 1};
 
 var liste_chemin = [];
 liste_chemin = generer();
+var liste_chemin2 = {liste : liste_chemin};  // une alternative pour les doublons
+var b = liste_chemin2.liste;
+
+incrementeur = 0;
 
 //choix aleatoire des cartes au début
 carte_depart_banque.src=change(liste_chemin);
 carte_depart_joueur.src=change(liste_chemin); 
+
+
+
 
 // bouton carte joueur
 element1.addEventListener('click',function ajoutImgDansDiv_2() {
@@ -60,7 +61,11 @@ element1.addEventListener('click',function ajoutImgDansDiv_2() {
     var newImg = creer(chemin);
 	var divJS = document.getElementById('carte_joueur');
     divJS.appendChild(newImg);
+    b.splice(points.valeur,1);
 });
+
+
+
 
 // bouton carte banque	
 element2.addEventListener('click',function ajoutImgDansDiv_2() {
@@ -69,6 +74,29 @@ element2.addEventListener('click',function ajoutImgDansDiv_2() {
 	var divJS = document.getElementById('carte_banque');
     divJS.appendChild(newImg);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//augmentation des points
 
 /*
  var liste_1 = [2-10];
